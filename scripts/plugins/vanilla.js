566548006e851c999937fd027960d53e5a09faf8
PabloSepu <67283131+PabloSepu@users.noreply.github.com>
2024-08-19 14:01:11 -0400

    Update vanilla.js
    dep warnings on foundry.utils.getProperty

--- scripts/plugins/vanilla.js
+++ scripts/plugins/vanilla.js
@@ -12,7 +12,7 @@ export default {
 
 			if (!lib.getSetting(CONSTANTS.SETTING_KEYS.ENABLE_GROUP_ATTACKS)) return true;
 
-			const isGroupAttack = getProperty(item, CONSTANTS.FLAGS.MIDI_GROUP_ATTACK) ?? false;
+			const isGroupAttack = foundry.utils.getProperty(item, CONSTANTS.FLAGS.MIDI_GROUP_ATTACK) ?? false;
 
 			if (!api.isMinion(item.parent) || !isGroupAttack) return true;
 
@@ -71,7 +71,7 @@ export default {
 
 			if (item.system?.damage?.parts?.length < 1) return true;
 			if (!lib.getSetting(CONSTANTS.SETTING_KEYS.ENABLE_GROUP_ATTACKS)) return true;
-			const isGroupAttack = getProperty(item, CONSTANTS.FLAGS.MIDI_GROUP_ATTACK) ?? false;
+			const isGroupAttack = foundry.utils.getProperty(item, CONSTANTS.FLAGS.MIDI_GROUP_ATTACK) ?? false;
 			if (!api.isMinion(item.parent) || !isGroupAttack) return true;
 
 			if (minionAttacks?.[item.parent.uuid] && minionAttacks?.[item.parent.uuid].attacked) {
@@ -116,9 +116,9 @@ export default {
 		Hooks.on("dnd5e.preUseItem", (item, config, options) => {
 			if (!lib.getSetting(CONSTANTS.SETTING_KEYS.ENABLE_GROUP_ATTACKS)) return true;
 
-			const isGroupAttack = getProperty(item, CONSTANTS.FLAGS.MIDI_GROUP_ATTACK) ?? false;
+			const isGroupAttack = foundry.utils.getProperty(item, CONSTANTS.FLAGS.MIDI_GROUP_ATTACK) ?? false;
 
-			const isScaleDC = getProperty(item, CONSTANTS.FLAGS.DC_SCALING_ENABLED) ?? false;
+			const isScaleDC = foundry.utils.getProperty(item, CONSTANTS.FLAGS.DC_SCALING_ENABLED) ?? false;
 
 			if (!api.isMinion(item.parent) || !isGroupAttack || !isScaleDC) return true;
 
@@ -176,7 +176,7 @@ export default {
 
 			let damageTotal = damageRoll.total;
 
-			const minionHP = getProperty(hitTarget.actor, "system.attributes.hp.value");
+			const minionHP = foundry.utils.getProperty(hitTarget.actor, "system.attributes.hp.value");
 
 			if ((minionHP - damageTotal) > 0) return true;
 
